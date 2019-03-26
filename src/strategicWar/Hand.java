d263621a4475020c4da06a1dcbaf17bbf08bf3dd
Ryan <rch827@gmail.com>
2019-03-25 22:03:53 -0400

    Hand Functions
    Finished the Hand functions

--- src/strategicWar/Hand.java
+++ src/strategicWar/Hand.java
@@ -1,9 +1,59 @@
 package strategicWar;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Hand {
-
+	
+	ArrayList<Card> hand;
+	/**
+	 * Default constructor for the Hand class. Hand has no Cards in it.
+	 */
 	public Hand() {
-		// TODO Auto-generated constructor stub
+		hand = new ArrayList<Card>();
 	}
-
+	/**
+	 * Initializes the Hand with the set of Cards
+	 * @param cards Set of Cards to be added into the Hand
+	 */
+	public void initialize(ArrayList<Card> cards) {
+		for(Card c : cards) {
+			hand.add(c);
+		}
+	}
+	/**
+	 * Returns how many Cards are in the Hand
+	 * @return Amount of Cards in the Hand
+	 */
+	public int cardCount() {
+		return hand.size();
+	}
+	/**
+	 * Adds the Card into the end of the Hand
+	 * @param card Card to be added
+	 */
+	public void addCard(Card card) {
+		hand.add(card);
+	}
+	
+	
+	/**
+	 * Takes a number from 0 to 4, removes and returns that Card.
+	 * @param i The card number (0 to 4)
+	 * @return The Card corresponding to i
+	 */
+	public Card selectCard(int i) {
+		return hand.remove(i);
+	}
+	
+	
+	/**
+	 * Picks a random card from the Hand, removes it and returns it.
+	 * @return A random card from the Hand
+	 */
+	public Card pickRandom() {
+		Random r = new Random();
+		int i = r.nextInt(hand.size());
+		return hand.remove(i);
+	}
 }
--- src/strategicWar/HandTest.java
+++ src/strategicWar/HandTest.java
@@ -2,13 +2,59 @@ package strategicWar;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class HandTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConstructor() {
+		Hand hand = new Hand();
+		assertTrue(hand.cardCount() == 0);
+	}
+	
+	@Test
+	public void testInitialize() {
+		Hand hand = new Hand();
+		ArrayList<Card> cards = new ArrayList<Card>();
+		for(int i = 0; i < 4; i++) {
+			cards.add(new Card(i,i));
+		}
+		hand.initialize(cards);
+		assertTrue(hand.cardCount() == 4);
+	}
+	
+	@Test
+	public void testAddCard() {
+		Hand hand = new Hand();
+		hand.addCard(new Card(1, 1));
+		assertTrue(hand.cardCount() == 1);
+		assertTrue(hand.selectCard(0).getSuite() == 1);
+	}
+	
+	@Test
+	public void testCardCount() {
+		Hand hand = new Hand();
+		hand.addCard(new Card(1, 1));
+		assertTrue(hand.cardCount() == 1);
+	}
+	
+	@Test
+	public void testSelectCard() {
+		Hand hand = new Hand();
+		hand.addCard(new Card(1, 1));
+		assertTrue(hand.selectCard(0).getSuite() == 1);
+	}
+	
+	@Test
+	public void testPickRandom() {
+		Hand hand = new Hand();
+		for(int i = 0; i < 4; i++) {
+			hand.addCard(new Card(i,i));
+		}
+		Card c = hand.pickRandom();
+		assertTrue(hand.cardCount() == 3);
 	}
 
 }
