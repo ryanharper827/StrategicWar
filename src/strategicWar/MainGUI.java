c1efa2c6172d210ec84fce0da1e57240cdb05648
Ryan <rch827@gmail.com>
2019-04-03 21:51:31 -0400

    Style Update
    Created CSS style file that dictates styling used for the program and implemented styling of buttons, labels, and a background image.

--- src/strategicWar/MainGUI.java
+++ src/strategicWar/MainGUI.java
@@ -5,14 +5,20 @@ import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.event.ActionEvent;
-import sun.tools.jstat.Alignment;
 
 import java.util.Stack;
 
@@ -27,41 +33,52 @@ public class MainGUI{
         HBox titlePane = new HBox();
         titlePane.setAlignment(Pos.CENTER);
         Label title = new Label("Strategic War");
+        title.setId("title-label");
         titlePane.getChildren().add(title);
 
         this.firstPane = new VBox();
         this.firstPane.setAlignment(Pos.CENTER);
         this.firstPane.setSpacing(25);
+        
         Button playButton = new Button("New Game");
-        playButton.setPrefSize(150,75);
+        playButton.setPrefSize(300,75);
         playButton.setOnAction(e -> transitionDifficultyPane());
+        
         Button exitButton = new Button("Exit");
         exitButton.setPrefSize(150,75);
-        //exitButton.setOnAction(buttonEvents[1]);
+        exitButton.setOnAction(e -> System.exit(0));
+        
         this.firstPane.getChildren().addAll(playButton, exitButton);
 
         this.difficultyPane = new VBox();
         this.difficultyPane.setAlignment(Pos.CENTER);
         this.difficultyPane.setSpacing(25);
+        
         Label diffLabel = new Label("Select Difficulty");
+        diffLabel.setId("diff-label");
         Button easyButton = new Button("Easy");
-        easyButton.setPrefSize(150,75);
-
+        easyButton.setPrefSize(300,75);
+        //TODO Implement easyButton to create a new game
+        //easyButton.setOnAction(e -> newGame(EASY);
+        
         Button hardButton = new Button("Hard");
-        hardButton.setPrefSize(150,75);
-
+        hardButton.setPrefSize(300,75);
+        //TODO Implement hardButton to create a new game
+        //hardButton.setOnAction(e -> newGame(HARD);
+        
         Button backButton = new Button("Back");
-        backButton.setPrefSize(150,75);
+        backButton.setPrefSize(300,75);
         backButton.setOnAction(e -> transitionFirstPane());
 
         this.difficultyPane.getChildren().addAll(diffLabel, easyButton, hardButton, backButton);
 
-
+        
         this.masterPane = new BorderPane();
         this.masterPane.setTop(titlePane);
         this.masterPane.setCenter(this.firstPane);
-
+        this.masterPane.setBackground(new Background(new BackgroundImage(new Image("strategicWar/background.jpg", true), BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT)));
         this.scene = new Scene(masterPane, 1000, 750);
+        this.scene.getStylesheets().add("strategicWar/style.css");
     }
 
     public Scene getScene()
